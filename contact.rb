51f1badb66356b59cc0888a14de94a466e146d50
Your Name <arssonist@yahoo.com>
2016-08-16 19:50:36 -0400

    another commit of contacts

--- contact.rb
+++ contact.rb
@@ -1,6 +1,6 @@
 class Contact
 
-  attr_reader :id
+  attr_reader :id, :contacts
   attr_accessor :first_name, :last_name, :email, :note
 
   @@contacts = []
@@ -12,8 +12,8 @@ class Contact
       @last_name = last_name
       @email = email
       @note = note
-    @id = @@id
-    @@id += 1
+      @id = @@id
+      @@id += 1
   end
 
   # This method should call the initializer,
@@ -24,7 +24,7 @@ class Contact
     return new_contact
   end
 
-  # This method should return all of the existing contacts
+  # This method should return all of the existing contact.
   def self.all
     @@contacts
   end
@@ -35,7 +35,7 @@ class Contact
     @@contacts.each do |object|
       if object.id == id
         return object
-    end
+      end
     end
   end
 
@@ -64,23 +64,42 @@ class Contact
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
+  def self.find_by(attribute_as_string, value)
+      @@contacts.each do |object|
+
+        if object.first_name == value && attribute_as_string == "first_name"
+          return object
+        elsif object.last_name == value && attribute_as_string == "last_name"
+          return object
+        elsif object.email == value && attribute_as_string == "email"
+          return object
+        elsif object.note == value && attribute_as_string == value && attribute_as_string == "note"
+          return object
+        else
+          return "Invalid input"
+        # else
+        #     return "Invalid entry"
+
+        end
+      end
 
   end
 
   # This method should delete all of the contacts
+  #This must be called on an instance of the conact class to empty the class contents.
   def self.delete_all
-
+      @@contacts = []
   end
 
   def full_name
+      @first_name + " " + @last_name
 
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    
   end
 
   # Feel free to add other methods here, if you need them.
