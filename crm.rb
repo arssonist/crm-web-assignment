979503bb3fa101f6121589eea84be13aab860369
Your Name <arssonist@yahoo.com>
2016-08-18 14:48:25 -0400

    web crm commit with program working

--- crm.rb
+++ crm.rb
@@ -2,14 +2,18 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 require 'sinatra'
+require_relative 'contact'
+#
+ get '/' do
+  @crm_app_name = "Chris's App"
+  @time = Time.new
+  erb :index
+end
 
-# get '/' do
-#   @crm_app_name = "Chris's App"
-#   @time = Time.new
-#   erb :index
-# end
+get '/contacts' do
+  Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+  Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+  Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
-get '/' do
-  "TEST"
   erb :contacts
 end
