1d0a4042483b334d7f3923cc225fe1999ca0e87a
Your Name <arssonist@yahoo.com>
2016-08-22 17:41:43 -0400

    commit of working but crappy looking web crm

--- crm.rb
+++ crm.rb
@@ -69,19 +69,28 @@ get '/contacts/:id/edit' do
     end
 end
 
-# non working code for editing a contact
+# to update
 put '/contacts/:id' do
-  @contact = Contact.find(params[:id].to_i)
-  if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
 
+  @contact = Contact.find(params[:id].to_i)
+  @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
     redirect to('/contacts')
-  else
-    raise Sinatra::NotFound
-  end
+
+  # if @contact
+  #   @contact.first_name = params[:first_name]
+  #   @contact.last_name = params[:last_name]
+  #   @contact.email = params[:email]
+  #   @contact.note = params[:note]
+
+  #   redirect to('/contacts')
+  # else
+  #   raise Sinatra::NotFound
+  # end
 end
 
 #
